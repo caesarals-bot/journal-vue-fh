9f3c8ba111958a46d084b4fb433e5ad963bcd125
cesar londoño <slac_@hotmail.com>
2021-11-09 09:10:46 -0300

    00002

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
+import  Store  from './store';
 import App from './App.vue'
 import router from './router'
 
 import  "./styles/styles.scss";
 
 createApp(App)
+    .use(Store)
     .use(router)
     .mount('#app')
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,25 @@
+
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date(). toDateString(), // sat 23,julio
+            texto: 'Elit amet magna irure consectetur proident voluptate laboris veniam Lorem Lorem laboris occaecat. Laborum commodo tempor elit do aute deserunt excepteur ex incididunt elit. Aliquip fugiat aute irure reprehenderit laboris velit non excepteur. Consectetur duis minim sit sint Lorem ipsum proident sint aliquip. Dolore Lorem anim consequat non occaecat dolore ad consectetur mollit duis nisi. Excepteur non reprehenderit pariatur adipisicing.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date(). toDateString(), // sat 23,julio
+            texto: 'Sunt deserunt non do esse culpa irure sit consequat ea culpa cupidatat ut. Exercitation deserunt sint velit officia laborum fugiat non occaecat ut eiusmod. Eiusmod deserunt minim aliquip in adipisicing ut commodo fugiat proident. Consectetur eu laboris velit ad incididunt exercitation nostrud fugiat consequat mollit ipsum dolore adipisicing.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date(). toDateString(), // sat 23,julio
+            texto: 'In amet nostrud commodo adipisicing. Sit adipisicing Lorem minim consectetur. Adipisicing et elit deserunt non. Proident aliquip nisi magna dolore laborum cillum consectetur deserunt non cupidatat ea laboris exercitation. Culpa duis mollit excepteur esse. Est amet mollit incididunt nostrud officia amet tempor aute enim. Pariatur ad nostrud irure aliqua.',
+            picture: null,
+        },
+    ]
+})
